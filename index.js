c8a9d911cf8164c40c86e97d476eedbe3af779c8
Jacksonckc <jackson858216047@gmail.com>
2022-09-13 17:49:08 -0600

    update app

--- index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('showWelcome').addEventListener('click', () => {
-  document.getElementById('showWelcome').style.display = 'none';
-  document.getElementById('welcome').innerHTML = 'Hello World';
-});
